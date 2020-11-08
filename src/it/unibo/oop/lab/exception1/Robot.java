1d2d31ae7ab18b150d85c527d2a2b4fa0030f5df
Matteo Guerra <matteo.guerra15@studio.unibo.it>
2020-11-08 11:16:10 +0100

    Completed es.lab06 exception 1

--- src/it/unibo/oop/lab/exception1/Robot.java
+++ src/it/unibo/oop/lab/exception1/Robot.java
@@ -33,27 +33,30 @@ public class Robot {
      * Moves the robot up by one unit.
      * 
      * @return If the Up movement has been performed
+     * @throws Exception 
      */
-    public boolean moveUp() {
-        return moveToPosition(environment.getCurrPosX(), this.environment.getCurrPosY() + Robot.MOVEMENT_DELTA);
+    public void moveUp() throws Exception {
+        moveToPosition(environment.getCurrPosX(), this.environment.getCurrPosY() + Robot.MOVEMENT_DELTA);
     }
 
     /**
      * Moves the robot down by one unit.
      * 
      * @return If the Down movement has been performed
+     * @throws Exception 
      */
-    public boolean moveDown() {
-        return this.moveToPosition(this.environment.getCurrPosX(), environment.getCurrPosY() - Robot.MOVEMENT_DELTA);
+    public void moveDown() throws Exception {
+        this.moveToPosition(this.environment.getCurrPosX(), environment.getCurrPosY() - Robot.MOVEMENT_DELTA);
     }
 
     /**
      * Moves the robot left by one unit.
      * 
      * @return A boolean indicating if the Left movement has been performed
+     * @throws Exception 
      */
-    public boolean moveLeft() {
-        return this.moveToPosition(this.environment.getCurrPosX() - Robot.MOVEMENT_DELTA,
+    public void moveLeft() throws Exception {
+        this.moveToPosition(this.environment.getCurrPosX() - Robot.MOVEMENT_DELTA,
                 this.environment.getCurrPosY());
     }
 
@@ -61,9 +64,10 @@ public class Robot {
      * Moves the robot right by one unit.
      * 
      * @return A boolean indicating if the Right movement has been performed
+     * @throws Exception 
      */
-    public boolean moveRight() {
-        return this.moveToPosition(this.environment.getCurrPosX() + Robot.MOVEMENT_DELTA,
+    public void moveRight() throws Exception {
+        this.moveToPosition(this.environment.getCurrPosX() + Robot.MOVEMENT_DELTA,
                 this.environment.getCurrPosY());
     }
 
@@ -81,24 +85,17 @@ public class Robot {
      *            the new X position to move the robot to
      * @param newY
      *            the new Y position to move the robot to
-     * @return true if robot gets moved, false otherwise
+     * 
+     * @throws Exception 
      */
-    private boolean moveToPosition(final int newX, final int newY) {
-        boolean returnValue = true;
+    private void moveToPosition(final int newX, final int newY) throws NotEnoughBatteryException {
         if (this.isBatteryEnoughToMove()) {
-            if (this.environment.move(newX, newY)) {
-                this.consumeBatteryForMovement();
-                this.log("Moved to position(" + newX + "," + newY + ").");
-            } else {
-                this.log("Can not move to (" + newX + "," + newY
-                        + ") the robot is touching at least one world boundary");
-                returnValue = false;
-            }
+        	this.environment.move(newX, newY);
+            this.consumeBatteryForMovement();
+            this.log("Moved to position(" + newX + "," + newY + ").");
         } else {
-            this.log("Can not move to position(" + newX + "," + newY + "). Not enough battery.");
-            returnValue = false;
+        	throw new NotEnoughBatteryException(batteryLevel, MOVEMENT_DELTA_CONSUMPTION);
         }
-        return returnValue;
     }
 
     /**
